c026f672ef0d798740f8b01afaec5567d4664dbb
Troels Henningsen <elektroels@gmail.com>
2022-01-20 11:45:25 +0100

    det naermer sig en sammenhaeng

---
+++ oev30.js
@@ -0,0 +1,7 @@
+// øvelse 4
+
+// Forrige: Loop en bold så det ligner bevægelse
+
+// Den her: En liste af bolde
+// Næste: Dernæst loop + requestAnimationFrame sådan at vi kan lave flere streger
+// Næste næste: Dernæst den sorte baggrund sådan at det ligner bolde
